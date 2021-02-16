83039268308b0e46097588f7c5e1bb51e8c6f648
Jan Schwien <jan5366@gmail.com>
2021-02-16 23:41:06 +0100

    Add grain item

--- Factorio-Agriculture/prototypes/item.lua
+++ Factorio-Agriculture/prototypes/item.lua
@@ -1,7 +1,15 @@
 data:extend(
         {
             {
-
+                {
+                    type = "item",
+                    name = "grain",
+                    icon = "__factorio-agriculture__/graphics/icons/placeholder.png",
+                    icon_size = 64, icon_mipmaps = 4,
+                    subgroup = "raw-resource",
+                    order = "a[grain]",
+                    stack_size = 100
+                }
             }
         }
 )
