9b6823be76e6292f6a3e20e0cf33c64090d14ab0
Jan Schwien <jan5366@gmail.com>
2021-05-28 08:58:40 +0200

    Adjust base dependency, Adjust changelog

--- Factorio-Agriculture/control.lua
+++ Factorio-Agriculture/control.lua
@@ -3,7 +3,6 @@ script.on_load(function()
     script.on_event(defines.events.on_built_entity, built_entity_function)
 end)
 
-
 function built_entity_function(event)
     local player = game.get_player(event.player_index)
     player.print("Built! YEAY!")
