85cfeedfa7ab1bf16d43a82edb4f38e9cfc6a980
Jan Schwien <jan5366@gmail.com>
2021-06-12 10:54:54 +0200

    Fix biogas entry

--- Factorio-Agriculture/prototypes/fluid.lua
+++ Factorio-Agriculture/prototypes/fluid.lua
@@ -15,13 +15,14 @@ data:extend(
         type = "fluid",
         name = "biogas",
         default_temperature = 45,
+        max_temperature = 65,
         heat_capacity = "0.1KJ",
         base_color = {r=0.34, g=0.34, b=0.1},
         flow_color = {r=0.7, g=0.7, b=0.7},
         icon = "__factorio-agriculture__/graphics/item/placeholder.png",
         icon_size = 64, icon_mipmaps = 4,
-        order = "a[fluid]-a[biogas]"
-        gas_temperature = 45,
+        order = "a[fluid]-a[biogas]",
+        gas_temperature = 15,
         auto_barrel = false
     }
 })
